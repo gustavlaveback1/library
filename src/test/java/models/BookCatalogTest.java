44f7356827e0c4b593b9d59ff81e7b27b575d3f5
gustavlaveback1 <ac75138@skola.goteborg.se>
2025-11-22 15:06:42 +0100

    First commit, copied all the files and added target

---
+++ src/test/java/models/BookCatalogTest.java
@@ -0,0 +1,49 @@
+package models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class BookCatalogTest {
+
+	private BookCatalog bc;
+	private Book book1;
+
+	public BookCatalogTest() {
+		bc = new BookCatalog();
+		book1 = new Book(1,"Learning Java","","","",0);
+		bc.addBook(book1);
+	}
+
+	//G
+	@Test
+	public void testAddABook() {
+
+	}
+
+	//G
+	@Test
+	public void testFindBook() {
+
+	}
+
+	//G
+	@Test
+	public void testFindBookIgnoringCase() {
+
+	}
+
+	//G
+	@Test
+	public void testFindBookWithExtraSpaces() {
+
+	}
+
+	//VG
+	// This test should throw BookNotFoundException in order to pass.
+	@Test
+	public void testFindBookThatDoesntExist() throws BookNotFoundException {
+
+	}
+
+}
---
+++ src/test/java/models/BookTest.java
@@ -0,0 +1,21 @@
+package models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookTest {
+
+	//G
+	@Test
+	public void test2EqualBooks() {
+
+	}
+
+	//G
+	@Test
+	public void test2NonEqualBooks() {
+
+	}
+
+}
