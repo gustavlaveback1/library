1364d8130ddd0c9355a8591871c70c66f3bfc5e4
gustavlaveback1 <ac75138@skola.goteborg.se>
2025-11-23 14:13:55 +0100

    added tests for BookCatalog, 1 more before finished

--- src/test/java/models/BookCatalogTest.java
+++ src/test/java/models/BookCatalogTest.java
@@ -2,6 +2,8 @@ package models;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BookCatalogTest {
@@ -9,28 +11,33 @@ public class BookCatalogTest {
 	private BookCatalog bc;
 	private Book book1;
 
+
 	public BookCatalogTest() {
 		bc = new BookCatalog();
 		book1 = new Book(1,"Learning Java","","","",0);
 		bc.addBook(book1);
 	}
-
 	//G
 	@Test
 	public void testAddABook() {
+        Book book1 = new Book(10, "Harry Potter", "JK Rowling", "12244", "21", 21);
+	    bc.addBook(book1);
+    }
 
+	//G
+	@Test
+	public void testFindBook() throws BookNotFoundException {
+        Book found = bc.findBook(book1.getTitle());
+        assertNotNull(found);
+        assertEquals(book1.getTitle(), found.getTitle());
 	}
 
 	//G
 	@Test
-	public void testFindBook() {
-
-	}
-
-	//G
-	@Test
-	public void testFindBookIgnoringCase() {
-
+	public void testFindBookIgnoringCase() throws BookNotFoundException{
+        Book found = bc.findBook("LEARNING JAVA");
+        assertNotNull(found);
+        assertEquals(book1.getTitle(), found.getTitle());
 	}
 
 	//G
