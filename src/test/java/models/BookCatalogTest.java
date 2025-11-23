81837fc4f66aa442da44bad72080ae614e4e445e
gustavlaveback1 <ac75138@skola.goteborg.se>
2025-11-23 14:19:07 +0100

    removed unnecessary imports

--- src/test/java/models/BookCatalogTest.java
+++ src/test/java/models/BookCatalogTest.java
@@ -1,9 +1,6 @@
 package models;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class BookCatalogTest {
