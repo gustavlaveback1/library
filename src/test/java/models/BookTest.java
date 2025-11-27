dd2602f977bf3a7837d6452f205b9f13d06e05bb
gustavlaveback1 <ac75138@skola.goteborg.se>
2025-11-27 14:49:18 +0100

    fixed book tests

--- src/test/java/models/BookTest.java
+++ src/test/java/models/BookTest.java
@@ -12,18 +12,15 @@ class BookTest {
     public void test2EqualBooks() {
         Book book1 = new Book(10, "Harry Potter", "JK Rowling", "12244", "21", 21);
         Book book2 = new Book(10, "Harry Potter", "JK Rowling", "12244", "21", 21);
-        assertEquals(book1.getTitle(), book2.getTitle());
-        assertEquals(book1.getAuthor(), book2.getAuthor());
-        assertEquals(book1.getIsbn(), book2.getIsbn());
+        assertEquals(book1, book2);
     }
 
     //G
     @Test
     public void test2NonEqualBooks() {
-        Book book1 = new Book(10, "Harry Potter", "JK Rowling", "12244", "21", 21);
-        Book book2 = new Book(10, "Sagan om ringen", "JRR Tolkien", "12244", "21", 21);
-        assertNotEquals(book1.getTitle(), book2.getTitle());
-        assertNotEquals(book1.getAuthor(), book2.getAuthor());
+        Book book1 = new Book(1, "Harry Potter", "JK Rowling", "12244", "21", 21);
+        Book book2 = new Book(2, "Harry Potter", "JK Rowling", "12244", "21", 21);
+        assertNotEquals(book1, book2);
     }
 
 }
